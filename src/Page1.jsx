f924c4ce090f45f222de44ac05b90ba8f9afb583
vdeepak19 <deepak@kluniversity.in>
2025-07-23 09:44:17 +0530

    V2

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class Page1 extends Component {
   render() {
     return (
+      <div color='blue'>
       <div>Page1</div>
+      </div>
     )
   }
 }
